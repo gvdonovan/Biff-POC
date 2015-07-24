d4eb806342c14b81f00f579b8099719384edcf23
jadams <jadams@optimalblue.com>
2015-07-24 17:14:16 -0500

    unit tests

--- src/client/tests/search/search.tests.js
+++ src/client/tests/search/search.tests.js
@@ -1,16 +1,29 @@
 'use strict';
 
-describe('SearchController', function () {
-    var searchController;
+describe('Testing SearchController', function () {
+    var searchController, mockedSearchService, deferred, rootScope;
     //mock Application to allow us to inject our own dependencies
     beforeEach(angular.mock.module('app.search'));
+
     //mock the controller for the same reason and include $rootScope and $controller
-    beforeEach(angular.mock.inject(function ($controller) {
+    beforeEach(angular.mock.inject(function ($controller, _quickSearchService_, $q, _$rootScope_) {
+
         //declare the controller since we are using controller as we do not need scope
-        searchController = $controller('SearchController', {});
+        searchController = $controller('SearchController');
+        // init the mock
+        mockedSearchService = _quickSearchService_;
+        deferred = $q;
+        rootScope = _$rootScope_;
+
     }));
     // tests start here
     it('should have a title of Quick Search', function () {
+        //create a watcher on the getResults method of the service and call through the promise
+        spyOn(mockedSearchService, 'getResults').and.callThrough();
+
         expect(searchController.title).toBe('Quick Search');
+        searchController.submit();
+        rootScope.$digest();//needed for expect to work correctly
+        expect(mockedSearchService.getResults).toHaveBeenCalled();
     });
 });
