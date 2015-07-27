f316f5672cbac42941e2fc1b67aeeeade3d5e58c
jadams <jadams@optimalblue.com>
2015-07-27 14:34:09 -0500

    gulp test

---
+++ build/tasks/test.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var karma = require('karma').server;
+var paths   = require('../paths');
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+    karma.start({
+        configFile:  __dirname + '/../../karma.conf.js',
+        singleRun: true
+    }, function(e) {
+        done();
+    });
+});
+
+/**
+ * Watch for file changes and re-run tests on each change
+ */
+gulp.task('tdd', function (done) {
+    karma.start({
+        configFile: __dirname + '/../../karma.conf.js'
+    }, function(e) {
+        done();
+    });
+});
