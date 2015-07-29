208bb300c99b38f738b9e2f0dde764c24536423b
jadams <jadams@optimalblue.com>
2015-07-29 10:15:34 -0500

    es6 class

--- src/client/es6/account.js
+++ src/client/es6/account.js
@@ -1,14 +1,36 @@
 class Account {
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    constructor(name, phone, fax, site, address1, address2, city, zip) {
+        this.name = name;
+        this.phone = phone;
+        this.fax = fax;
+        this.site = site;
+        this.address1 = address1;
+        this.address2 = address2;
+        this.city = city;
+        this.zip = zip;
     }
-    set firstName (firstName){ this._firstName = firstName }
-    get firstName (){ return this._firstName }
-    set lastName (lastName){ this._lastName = lastName }
-    get lastName (){ return this._lastName }
+    set name (name){ this._name = name }
+    get name (){ return this._name }
 
-    get fullName() {
-        return this.firstName + ' ' + this.lastName;
-    }
+    set phone (phone){ this._phone = phone }
+    get phone (){ return this._phone }
+
+    set fax (fax){ this._fax = fax}
+    get fax (){ return this._fax }
+
+    set site (site){ this._site = site }
+    get site (){ return this._site }
+
+    set address1 (address1){ this._address1 = address1 }
+    get address1 (){ return this._adress1}
+
+    set address2 (address2){ this._address2 = address2}
+    get address2 (){ return this._address2}
+
+    set city (city){ this._city = city}
+    get city (){return this._city}
+
+    set zip (zip){ this._zip = zip }
+    get zip (){ return this._zip}
+
 }
--- src/client/lib/account.js
+++ src/client/lib/account.js
@@ -1,37 +1,86 @@
-'use strict';
+"use strict";
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Account = (function () {
-    function Account(firstName, lastName) {
+    function Account(name, phone, fax, site, address1, address2, city, zip) {
         _classCallCheck(this, Account);
 
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.name = name;
+        this.phone = phone;
+        this.fax = fax;
+        this.site = site;
+        this.address1 = address1;
+        this.address2 = address2;
+        this.city = city;
+        this.zip = zip;
     }
 
     _createClass(Account, [{
-        key: 'firstName',
-        set: function set(firstName) {
-            this._firstName = firstName;
+        key: "name",
+        set: function set(name) {
+            this._name = name;
         },
         get: function get() {
-            return this._firstName;
+            return this._name;
         }
     }, {
-        key: 'lastName',
-        set: function set(lastName) {
-            this._lastName = lastName;
+        key: "phone",
+        set: function set(phone) {
+            this._phone = phone;
         },
         get: function get() {
-            return this._lastName;
+            return this._phone;
         }
     }, {
-        key: 'fullName',
+        key: "fax",
+        set: function set(fax) {
+            this._fax = fax;
+        },
         get: function get() {
-            return this.firstName + ' ' + this.lastName;
+            return this._fax;
+        }
+    }, {
+        key: "site",
+        set: function set(site) {
+            this._site = site;
+        },
+        get: function get() {
+            return this._site;
+        }
+    }, {
+        key: "address1",
+        set: function set(address1) {
+            this._address1 = address1;
+        },
+        get: function get() {
+            return this._adress1;
+        }
+    }, {
+        key: "address2",
+        set: function set(address2) {
+            this._address2 = address2;
+        },
+        get: function get() {
+            return this._address2;
+        }
+    }, {
+        key: "city",
+        set: function set(city) {
+            this._city = city;
+        },
+        get: function get() {
+            return this._city;
+        }
+    }, {
+        key: "zip",
+        set: function set(zip) {
+            this._zip = zip;
+        },
+        get: function get() {
+            return this._zip;
         }
     }]);
 
