d6b1e7bf7f9fd2f2a89250aca7a089fb4e394db9
dhob15 <dhertenstein@optimalblue.com>
2015-07-30 11:12:14 -0500

    Updates to add in the dropdown directive to the examples section.

---
+++ src/client/app/obui/controllers/obuiDropdown.controller.js
@@ -0,0 +1,38 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.obui')
+        .controller('obuiDropdownController', ObuiDropdownController);
+  
+    ObuiDropdownController.$inject = [];
+      
+    // Datepicker
+    /* @ngInject */
+    function ObuiDropdownController() {
+      
+        var vm = this;  
+      
+        vm.items = [
+          'The first choice!',
+          'And another choice for you.',
+          'but wait! A third!'
+        ];
+
+        vm.status = {
+          isopen: false
+        };
+
+        vm.toggled = function(open) {
+          // $log.log('Dropdown is now: ', open);
+        };
+
+        vm.toggleDropdown = function($event) {
+          $event.preventDefault();
+          $event.stopPropagation();
+          vm.status.isopen = !vm.status.isopen;
+        };
+      
+    }
+  
+})();
