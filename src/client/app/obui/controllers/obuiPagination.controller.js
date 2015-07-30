78a1a6eccee3699b6c9a1b5ff841d9512a13716f
dhob15 <dhertenstein@optimalblue.com>
2015-07-30 12:16:51 -0500

    Added the pagination directives and markup. Looks pretty good with the different options etc.

---
+++ src/client/app/obui/controllers/obuiPagination.controller.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.obui')
+        .controller('obuiPaginationController', ObuiPaginationController);
+  
+    ObuiPaginationController.$inject = [];
+      
+    // Datepicker
+    /* @ngInject */
+    function ObuiPaginationController() {
+      
+        var vm = this;  
+      
+        vm.totalItems = 64;
+        vm.currentPage = 4;
+
+        vm.setPage = function (pageNo) {
+          vm.currentPage = pageNo;
+        };
+
+        vm.pageChanged = function() {
+          // $log.log('Page changed to: ' + vm.currentPage);
+        };
+
+        vm.maxSize = 5;
+        vm.bigTotalItems = 175;
+        vm.bigCurrentPage = 1;
+      
+    }
+    
+  
+})();
