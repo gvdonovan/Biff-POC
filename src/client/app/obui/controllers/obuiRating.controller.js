24368c03c1e1b6b7d390a93af2fbb8564070085d
dhob15 <dhertenstein@optimalblue.com>
2015-07-30 15:17:15 -0500

    Rating directive. Has a nice tooltip on rollover with the amounts with colors.

---
+++ src/client/app/obui/controllers/obuiRating.controller.js
@@ -0,0 +1,36 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.obui')
+        .controller('obuiRatingController', ObuiRatingController);
+  
+    ObuiRatingController.$inject = [];
+      
+    // Datepicker
+    /* @ngInject */
+    function ObuiRatingController() {
+      
+        var vm = this;  
+      
+        vm.rate = 7;
+        vm.max = 10;
+        vm.isReadonly = false;
+
+        vm.hoveringOver = function(value) {
+          vm.overStar = value;
+          vm.percent = 100 * (value / vm.max);
+        };
+
+        vm.ratingStates = [
+          {stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
+          {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
+          {stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
+          {stateOn: 'glyphicon-heart'},
+          {stateOff: 'glyphicon-off'}
+        ];
+      
+    }
+    
+  
+})();
