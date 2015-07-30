2bcbc6196b54dd72ab5f72daf83cdc58baa9661a
dhob15 <dhertenstein@optimalblue.com>
2015-07-30 11:32:40 -0500

    Modal directive files and source code. This includes a template script and an inner modal controller which is a good example.

---
+++ src/client/app/obui/controllers/obuiModal.controller.js
@@ -0,0 +1,69 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.obui')
+        .controller('obuiModalController', ObuiModalController);
+  
+    ObuiModalController.$inject = ['$modal'];
+      
+    // Datepicker
+    /* @ngInject */
+    function ObuiModalController($modal) {
+      
+        var vm = this;  
+      
+        vm.items = ['item1', 'item2', 'item3'];
+
+        vm.animationsEnabled = true;
+
+        vm.open = function (size) {
+
+          var modalInstance = $modal.open({
+            animation: vm.animationsEnabled,
+            templateUrl: 'myModalContent.html',
+            controller: 'ModalInstanceCtrl as vm',
+            size: size,
+            resolve: {
+              items: function () {
+                return vm.items;
+              }
+            }
+          });
+
+          modalInstance.result.then(function (selectedItem) {
+            vm.selected = selectedItem;
+          }, function () {
+            // $log.info('Modal dismissed at: ' + new Date());
+          });
+        };
+
+        vm.toggleAnimation = function () {
+          vm.animationsEnabled = !vm.animationsEnabled;
+        };
+
+      }
+
+      // Please note that $modalInstance represents a modal window (instance) dependency.
+      // It is not the same as the $modal service used above.
+
+      angular.module('app.obui').controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+        
+        var vm = this;
+
+        vm.items = items;
+        vm.selected = {
+          item: vm.items[0]
+        };
+
+        vm.ok = function () {
+          $modalInstance.close(vm.selected.item);
+        };
+
+        vm.cancel = function () {
+          $modalInstance.dismiss('cancel');
+        };
+      });     
+    
+  
+})();
