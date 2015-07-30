dc85b6e16b8b2b012b08985d92e3c934f4af9b6e
dhob15 <dhertenstein@optimalblue.com>
2015-07-30 15:23:22 -0500

    Tabs directive code source.

---
+++ src/client/app/obui/controllers/obuiTabs.controller.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.obui')
+        .controller('obuiTabsController', ObuiTabsController);
+  
+    ObuiTabsController.$inject = ['$window'];
+      
+    // Datepicker
+    /* @ngInject */
+    function ObuiTabsController($window) {
+      
+        var vm = this;  
+      
+        vm.tabs = [
+          { title:'Dynamic Title 1', content:'Dynamic content 1' },
+          { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+        ];
+
+        vm.alertMe = function() {
+          setTimeout(function() {
+            $window.alert('You\'ve selected the alert tab!');
+          });
+        };
+      
+    }
+    
+  
+})();
