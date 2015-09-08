6bc177a504e11e30ecd98e7d49179f2fff44f16a
jadams <jadams@optimalblue.com>
2015-09-08 10:53:33 -0500

    list page

--- src/client/app/quickSearchConfig/controllers/formList.js
+++ src/client/app/quickSearchConfig/controllers/formList.js
@@ -1,20 +1,37 @@
 (function () {
-        'use strict';
+    'use strict';
 
-        angular
-            .module('app.quickSearchConfig')
-            .controller('FormListController', FormListController);
+    angular
+        .module('app.quickSearchConfig')
+        .controller('FormListController', FormListController);
 
-        FormListController.$inject = ['logger', '$stateParams', '$state'];
-        /* @ngInject */
-        function FormListController(logger, $stateParams, $state) {
-            var vm = this;
-            vm.add = add;
-
-            function add() {
-                //Request then next page
-                $state.go('quickSearchConfigInputs', {});
+    FormListController.$inject = ['logger', '$stateParams', '$state'];
+    /* @ngInject */
+    function FormListController(logger, $stateParams, $state) {
+        var vm = this;
+        vm.add = add;
+        vm.forms = [
+            {
+                id: 1,
+                name: 'form1',
+                active: true
+            },
+            {
+                id: 2,
+                name: 'form2',
+                active: false
+            },
+            {
+                id: 3,
+                name: 'form3',
+                active: false
             }
+        ];
 
+        function add() {
+            //Request then next page
+            $state.go('quickSearchConfigInputs', {});
         }
-    })();
+
+    }
+})();
