478f2b0df684a604c2a399a5b420baf8b56c5294
dhob15 <dhertenstein@optimalblue.com>
2015-09-08 14:55:01 -0500

    Header and footer bindings for the inputs section.

--- src/client/app/quickSearchConfig/controllers/inputs.js
+++ src/client/app/quickSearchConfig/controllers/inputs.js
@@ -1,139 +1,144 @@
 //TODO: DH
 (function () {
-        'use strict';
+    'use strict';
 
-        angular
-            .module('app.quickSearchConfig')
-            .controller('InputsController', InputsController);
+    angular
+        .module('app.quickSearchConfig')
+        .controller('InputsController', InputsController);
 
-        InputsController.$inject = ['logger', '$stateParams', '$state', 'inputsService'];
-        /* @ngInject */
-        function InputsController(logger, $stateParams, $state, inputs) {
-            var vm = this;
-            vm.editMode = false;
-            vm.formId = null;
-            vm.next = next;
-            vm.previous = previous;
-            
-            vm.title = 'Inputs';
-            vm.submit = submit;
-            vm.isLoading = false;
-            vm.showJson = false;
-            vm.json = "";
-            vm.searchResults = [];
-            vm.underScoreJson = "";
-            vm.formModel = {};
-            vm.formFields = [];
+    InputsController.$inject = ['logger', '$stateParams', '$state', 'inputsService'];
+    /* @ngInject */
+    function InputsController(logger, $stateParams, $state, inputs) {
+        var vm = this;
+        vm.editMode = false;
+        vm.formId = null;
+        vm.next = next;
+        vm.previous = previous;
 
-            vm.setPreview = setPreview;
-            vm.moveRowUp = moveRowUp;
-            vm.moveRowDown = moveRowDown;            
-            
-            activate();
+        vm.title = 'Inputs';
+        vm.header = "Quick Search";
+        vm.footer = "";
+        vm.submit = submit;
+        vm.isLoading = false;
+        vm.showJson = false;
+        vm.json = "";
+        vm.searchResults = [];
+        vm.underScoreJson = "";
+        vm.formModel = {};
+        vm.formFields = [];
 
-            function activate() {
-                logger.info('Activated Inputs View');
+        vm.setPreview = setPreview;
+        vm.moveRowUp = moveRowUp;
+        vm.moveRowDown = moveRowDown;
 
-                vm.editMode = $stateParams.editMode;
-                vm.formId = $stateParams.formId;
+        activate();
 
-                inputs.getFormConfig().then(function (data) {
-                    vm.data = data;
-                    vm.formFields = data.fields;
+        function activate() {
+            logger.info('Activated Inputs View');
 
-                    var visible = [];
-                    var order = [];
+            vm.editMode = $stateParams.editMode;
+            vm.formId = $stateParams.formId;
 
-                    for(var i=0; i<vm.formFields.length; i++) {
-                        visible.push(false);
-                        order[i] = i;
-                    }
+            inputs.getFormConfig().then(function (data) {
+                vm.data = data;
+                vm.formFields = data.fields;
 
-                    vm.formModel.visible = visible;
-                    vm.formModel.order = order;
+                var visible = [];
+                var order = [];
 
-                });
-
-
-            }
-
-            function setPreview(order) {
-                var fm = vm.formModel;
-                fm.visible[order] = !fm.visible[order];
-                console.log("Order: "+order+", State:"+fm.visible[order]);
-                console.log(JSON.stringify(vm.formModel).toString());
-            }
-
-            function moveRowUp(index) {
-                console.log("UP: Index: "+index);
-                var ff = vm.formFields;
-                var fo = vm.formModel.order;
-
-                var fItem = ff.slice(0)[index];
-                var oItem = fo.slice(0)[index];
-
-                if(index != 0) {
-                    ff.splice(index, 1);
-                    ff.splice(index-1, 0, fItem);
-                    fo.splice(index, 1);
-                    fo.splice(index-1, 0, oItem);
-                } else {
-                    ff.shift();
-                    ff.push(fItem);
-                    fo.shift();
-                    fo.push(oItem);
+                for (var i = 0; i < vm.formFields.length; i++) {
+                    visible.push(false);
+                    order[i] = i;
                 }
-            }
 
-            function moveRowDown(index) {
-                console.log("DOWN: Index: "+index);
-                var ff = vm.formFields;
-                var fo = vm.formModel.order;
+                vm.formModel.visible = visible;
+                vm.formModel.order = order;
 
-                var fItem = ff.slice(0)[index];
-                var oItem = fo.slice(0)[index];
+            });
 
-                if(index+1 != ff.length) {
-                    ff.splice(index, 1);
-                    ff.splice(index+1, 0, fItem);
-                    fo.splice(index, 1);
-                    fo.splice(index+1, 0, oItem);
-                } else {
-                    ff.pop();
-                    ff.unshift(fItem);
-                    fo.pop();
-                    fo.unshift(oItem);
-                }
-            }
 
-            function submit() {
-                vm.isLoading = true;
-                return preview.getResults(vm.formModel).then(function (data) {
-                    vm.searchResults = data;
-                    vm.json = JSON.stringify(vm.formModel, null, 4);
-                    vm.showJson = true;
-                    vm.underScoreJson = underScoreFilter();
-                    $timeout(function () {
-                        vm.isLoading = false;
-                    }, 500);
-                });
-            }
+        }
 
-            function underScoreFilter() {
-                var biff = _.pluck(vm.searchResults, 'items');
-                var flat = _.flatten(biff);
-                var x = _.filter(flat, function (item) {
-                    return item.rebate >= 500;
-                });
-                return x;
-            }
+        function setPreview(order) {
+            var fm = vm.formModel;
+            fm.visible[order] = !fm.visible[order];
+            console.log("Order: " + order + ", State:" + fm.visible[order]);
+            console.log(JSON.stringify(vm.formModel).toString());
+        }
 
-            function next() {
-                $state.go('quickSearchConfigDefaults', {editMode: vm.editMode, formId: vm.formId});
-            }
+        function moveRowUp(index) {
+            console.log("UP: Index: " + index);
+            var ff = vm.formFields;
+            var fo = vm.formModel.order;
 
-            function previous() {
-                $state.go('quickSearchConfig');
+            var fItem = ff.slice(0)[index];
+            var oItem = fo.slice(0)[index];
+
+            if (index != 0) {
+                ff.splice(index, 1);
+                ff.splice(index - 1, 0, fItem);
+                fo.splice(index, 1);
+                fo.splice(index - 1, 0, oItem);
+            } else {
+                ff.shift();
+                ff.push(fItem);
+                fo.shift();
+                fo.push(oItem);
             }
         }
-    })();
+
+        function moveRowDown(index) {
+            console.log("DOWN: Index: " + index);
+            var ff = vm.formFields;
+            var fo = vm.formModel.order;
+
+            var fItem = ff.slice(0)[index];
+            var oItem = fo.slice(0)[index];
+
+            if (index + 1 != ff.length) {
+                ff.splice(index, 1);
+                ff.splice(index + 1, 0, fItem);
+                fo.splice(index, 1);
+                fo.splice(index + 1, 0, oItem);
+            } else {
+                ff.pop();
+                ff.unshift(fItem);
+                fo.pop();
+                fo.unshift(oItem);
+            }
+        }
+
+        function submit() {
+            vm.isLoading = true;
+            return preview.getResults(vm.formModel).then(function (data) {
+                vm.searchResults = data;
+                vm.json = JSON.stringify(vm.formModel, null, 4);
+                vm.showJson = true;
+                vm.underScoreJson = underScoreFilter();
+                $timeout(function () {
+                    vm.isLoading = false;
+                }, 500);
+            });
+        }
+
+        function underScoreFilter() {
+            var biff = _.pluck(vm.searchResults, 'items');
+            var flat = _.flatten(biff);
+            var x = _.filter(flat, function (item) {
+                return item.rebate >= 500;
+            });
+            return x;
+        }
+
+        function next() {
+            $state.go('quickSearchConfigDefaults', {
+                editMode: vm.editMode,
+                formId: vm.formId
+            });
+        }
+
+        function previous() {
+            $state.go('quickSearchConfig');
+        }
+    }
+})();
